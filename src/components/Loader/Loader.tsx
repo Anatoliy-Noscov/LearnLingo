abf5114a1778b221ce7d14968f8b4038dfa82a89
Anatoliy_Noscov <svodnuy777@gmail.com>
2025-11-03 23:12:55 +0200

    create Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './Loader.module.css';
+
+interface LoaderProps {
+    size?: 'small' | 'medium' | 'large';
+    color?: 'string';
+    text?: string;
+}
+
+export const Loader: React.FC<LoaderProps> = ({ 
+    size = 'medium',
+    color = '#0984e3',
+    text 
+}) => {
+    const getSize = () => {
+        switch (size) {
+            case 'small':
+                return '40px';
+            case 'large':
+                return '80px';
+            case 'medium':    
+            default: 
+            return '60px';
+        }
+    }
+    
+
+    return (
+
+        <div className={styles.loaderContainer}>
+            {/*Basic element loader */}
+            <div className={styles.spinner}
+            style={{
+                width: getSize(),
+                height: getSize(),
+                borderColor: color
+            }}>
+            </div>
+            {/**Условный рендеринг текста */}
+            {text && <p className={styles.loaderText}>{text}</p>}
+        </div>
+
+    )
+}
+
+export default Loader;
